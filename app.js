e4a013ea770197e26afbde0944a964c5af8f0121
Erwin Nahue <erwinnahue@gmail.com>
2024-01-24 15:40:21 -0500

    Add function intentoUsuario

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+let numeroSecreto = generarNumeroSecreto();
 
 function asignarTextolemento(elemento, texto) {
   let elementoHtml = document.querySelector(elemento);
   elementoHtml.innerHTML = texto;
 }
-function intentoDeUsuario() {
-  let numero = document.querySelector("input").value;
-  if (numero == 2) {
+function verificarIntento() {
+  let numeroUsuario = parseInt(document.getElementById("valorUsuario").value);
+  if (numeroUsuario === numeroSecreto) {
     asignarTextolemento("p", "¡Has acertado!");
   } else {
     asignarTextolemento("p", "Intenta otra vez");
   }
 }
+function generarNumeroSecreto(){
+  return Math.floor(Math.random() * 10) + 1;
+}
+
 asignarTextolemento("h1", "Adivina el número secreto");
 asignarTextolemento("p", "Escribe un número del 1 al 10");
