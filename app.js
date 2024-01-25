08f0f02f0b7e25ae5c1a377b2cff1e857f0d1cd9
Erwin Nahue <erwinnahue@gmail.com>
2024-01-24 22:45:19 -0500

    Refactor game logic and add restart functionality

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 let numeroSecreto = generarNumeroSecreto();
+let intentos = 1;
 
 function asignarTextolemento(elemento, texto) {
   let elementoHtml = document.querySelector(elemento);
   elementoHtml.innerHTML = texto;
 }
-function intentoDeUsuario() {
+function verificarIntento() {
   let numeroUsuario = parseInt(document.getElementById("valorUsuario").value);
   if (numeroUsuario === numeroSecreto) {
-    asignarTextolemento("p", "¡Has acertado!");
+    asignarTextolemento("p", `¡Has acertado! El número secreto era  ${numeroSecreto} y lo has adivinado en ${intentos} ${intentos === 1 ? "intento" : "intentos"}.`);
+
+    document.getElementById("reiniciar").removeAttribute('disabled');
+
   } else {
-    asignarTextolemento("p", "Intenta otra vez");
+    if (numeroUsuario > numeroSecreto) {
+      asignarTextolemento("p", "El número secreto es menor");
+    } else {
+      asignarTextolemento("p", "El número secreto es mayor");
+    }
+    intentos++;
+    limpiarInput();
   }
 }
-function generarNumeroSecreto(){
+function limpiarInput() {
+  document.getElementById("valorUsuario").value = "";
+}
+function reiniciarJuego() {
+  numeroSecreto = generarNumeroSecreto();
+  intentos = 1;
+  asignarTextolemento("p", "Escribe un número del 1 al 10");
+  limpiarInput();
+}
+function generarNumeroSecreto() {
   return Math.floor(Math.random() * 10) + 1;
 }
 
